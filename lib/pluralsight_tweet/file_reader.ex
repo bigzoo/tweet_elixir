63e23e33fde582827084db2e16d51ebd114d88ff
Chris Nyaga <nyagachris75@gmail.com>
2020-02-20 21:29:20 +0300

    Simple file reader implementation

---
+++ lib/pluralsight_tweet/file_reader.ex
@@ -0,0 +1,8 @@
+defmodule PluralsightTweet.FileReader do
+  def get_strings_to_tweet(path) do
+    File.read!(path)
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.trim/1)
+    |> Enum.filter(&String.length(&1) <= 140)
+  end
+end
